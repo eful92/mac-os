361a30d9032121cd21c445c52bedb27c6ef67a98
eful92 <57841108+eful92@users.noreply.github.com>
2022-03-30 04:54:52 +0700

    Update music.ts

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -2,9 +2,9 @@ import type { MusicData } from "../types";
 
 const music: MusicData = {
   title: "Sunflower",
-  artist: "Post Malone / Swae Lee",
+  artist: "Tulus / Hati-Hati Sayang",
   cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  audio: "sunflower.mp3"
 };
 
 export default music;
