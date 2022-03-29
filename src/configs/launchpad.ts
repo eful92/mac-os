aaec9d25d391b5772b1d22d6087b5101fb7c63ae
eful92 <57841108+eful92@users.noreply.github.com>
2022-03-30 05:24:19 +0700

    Update launchpad.ts

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,64 +2,40 @@ import type { LaunchpadData } from "../types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
+    id: "Magang 2021",
+    title: "Magang 2021",
     img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
+    link: ""
   },
   {
-    id: "metallic",
-    title: "Metallic",
+    id: "Tugas Web Lanjut",
+    title: "Tugas Web Lanjut",
     img: "img/icons/launchpad/meta.png",
-    link: "https://github.com/Renovamen/metallic"
+    link: ""
   },
   {
-    id: "alkaid",
-    title: "Alkaid",
+    id: "Skripsi 2022",
+    title: "Skripsi 2022",
     img: "img/icons/launchpad/rl.png",
-    link: "https://github.com/Renovamen/alkaid"
-  },
-  {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/icon.png",
-    link: "https://oh-vue-icons.netlify.app/"
-  },
-  {
-    id: "gungnir",
-    title: "Gungnir",
-    img: "img/icons/launchpad/gungnir.png",
-    link: "https://vuepress-theme-gungnir.vercel.app/"
-  },
-  {
-    id: "what-if",
-    title: "My Notebook",
-    img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
-  },
-  {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
+    link: ""
   },
   {
     id: "resume",
     title: "Résumé",
     img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
+    link: "h"
   },
   {
-    id: "zelda",
+    id: "Data Skripsi 2022",
     title: "Webpage for Zelda",
     img: "img/icons/launchpad/zelda.png",
-    link: "https://zelda.zxh.io/"
+    link: ""
   },
   {
-    id: "fishmail",
+    id: "My Project 2022",
     title: "Fishmail",
     img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
+    link: ""
   }
 ];
 
