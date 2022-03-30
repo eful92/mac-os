e4906cbc2a67b956e2dbe287608632e28da86065
eful92 <57841108+eful92@users.noreply.github.com>
2022-03-30 16:09:01 +0700

    Update VSCode.tsx

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="h-full w-full bg-vscode"
-      src="https://github1s.com/Renovamen/playground-macos/blob/main/README.md"
+      src="https://github1s.com/eful92/macos/README.md"
       frameBorder="0"
       title="VSCode"
     />
